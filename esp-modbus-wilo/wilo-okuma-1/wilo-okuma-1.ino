337655dd9b24087646fb8ac85ff914679212eb50
mesleki2017 <mesleki2017@gmail.com>
2023-12-11 14:18:06 +0300

    wilo modbus

---
+++ esp-modbus-wilo/wilo-okuma-1/wilo-okuma-1.ino
@@ -0,0 +1,73 @@
+/*
+  Modbus Library for Arduino Example - Modbus RTU Client
+  Read Holding Registers from Modbus RTU Server in blocking way
+  ESP8266 Example
+  
+  (c)2020 Alexander Emelianov (a.m.emelianov@gmail.com)
+  https://github.com/emelianov/modbus-esp8266
+*/
+
+
+#include <ModbusRTU.h>
+#include <SoftwareSerial.h>
+ModbusRTU mb;
+
+int DE_RE = 0; //D4  For MAX485 chip
+int RX = 12;
+int TX = 13;
+
+SoftwareSerial S(RX, TX);//D6/D7  (RX , TX)
+
+uint16_t Mread0[2];
+int my_array_size=sizeof(Mread0)/sizeof(uint16_t);
+int error_code=0;
+
+int cbWrite(Modbus::ResultCode event, uint16_t transactionId, void* data) {
+  //Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
+  // 0x00 = 0,  // Custom. No error
+  //0x02 =2 ,   // Output Address not exists
+  //0xE4 = 228, // Custom. Operation not finished within reasonable time
+  //Serial.println(event);
+  error_code=event;
+  return true;
+};
+
+float InttoFloat(uint16_t Data0,uint16_t Data1) {
+  float x;
+  unsigned long *p;
+  p = (unsigned long*)&x;
+  *p = (unsigned long)Data0 << 16 | Data1; //Big-endian
+  return(x);
+};
+
+void setup() {
+  Serial.begin(115200);
+  S.begin(19200, SWSERIAL_8N1);
+  mb.begin(&S, DE_RE); //Assing Software serial port to Modbus instance for MAX485 chip having DI,DE,RE,RO Pin at TTL side
+  mb.master(); //Assing Modbus function as master
+ 
+};
+
+void loop() {
+  //memset(Mread0, 0, sizeof(Mread0));// bu kod buffer bosaltma isine yaramadi!!!!
+  if (!mb.slave()) {
+    Serial.print("my_array_size ");Serial.println(my_array_size);
+    int start_register=30400;
+    mb.readHreg(3,start_register, Mread0,my_array_size-2, cbWrite);  //klemsan pdf (20 Phase 2 Voltage (L-N) float RO
+    Serial.print("error_code = ");Serial.println(error_code);
+
+    //Serial.println(InttoFloat(Mread0[0], Mread0[1]));
+    for (int aaa = 1; aaa < my_array_size; aaa=aaa+2) {
+      //Serial.print(Mread0[aaa-1]);Serial.print("--");Serial.println(Mread0[aaa]);
+     Serial.print(start_register+aaa-1);Serial.print("--");Serial.print(start_register+aaa); Serial.print(" =");
+     Serial.print(InttoFloat(Mread0[aaa-1], Mread0[aaa]));Serial.print("--");
+     Serial.print(Mread0[aaa-1]);Serial.print("--");Serial.println(Mread0[aaa]);
+      }
+    
+  }
+  mb.task();
+  delay(1000);
+  yield();
+  
+
+}
---
+++ rtu-port-tarama-3/rtu-port-tarama-3.ino
@@ -0,0 +1,116 @@
+/*
+  Modbus Library for Arduino Example - Modbus RTU Client
+  Read Holding Registers from Modbus RTU Server in blocking way
+  ESP8266 Example
+  
+  (c)2020 Alexander Emelianov (a.m.emelianov@gmail.com)
+  https://github.com/emelianov/modbus-esp8266
+*/
+
+
+#include <ModbusRTU.h>
+#include <SoftwareSerial.h>
+ModbusRTU mb;
+
+int DE_RE = 0; //D4  For MAX485 chip
+int RX = 12;
+int TX = 13;
+
+SoftwareSerial S(RX, TX);//D6/D7  (RX , TX)
+
+uint16_t Mread0[2];
+int my_array_size=sizeof(Mread0)/sizeof(uint16_t);
+int error_code=9999;
+
+int cihaz_id=0;
+int tarama_no=0;
+int myBaud[] = {9600,19200,38400,115200};
+Config myPar[] = {SWSERIAL_8N1,SWSERIAL_8N2};
+
+int cbWrite(Modbus::ResultCode event, uint16_t transactionId, void* data) {
+  //Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
+  // 0x00 = 0,  // Custom. No error
+  //0x02 =2 ,   // Output Address not exists
+  //0xE4 = 228, // Custom. Operation not finished within reasonable time
+  //Serial.println(event);
+  error_code=event;
+  return true;
+};
+
+float InttoFloat(uint16_t Data0,uint16_t Data1) {
+  float x;
+  unsigned long *p;
+  p = (unsigned long*)&x;
+  *p = (unsigned long)Data0 << 16 | Data1; //Big-endian
+  return(x);
+};
+
+String text1="";
+String data_to_string(String kkk,uint16_t myArray[], int length) {
+  text1=kkk;
+  for (byte i = 0; i < length; i++) {
+    //Serial.print(myArray[i]);Serial.print(",");
+    String l1n=String(myArray[i]);
+    text1=text1+","+l1n;
+  };
+  Serial.println(text1);
+  Serial.println(" ");
+  return text1;
+}
+void setup() {
+  ESP.eraseConfig();
+  Serial.begin(115200);
+  S.setTimeout(5000);//isime yaramadi;
+  S.begin(myBaud[0], myPar[0]);
+  Serial.print("myBaud = ");Serial.println(myBaud[0]);
+  Serial.print("myParity = ");Serial.println(myPar[0]);
+  uint32_t timeout = 100;
+  mb.setInterFrameTime(timeout);//isime yaramadi;
+
+  mb.master(); //Assing Modbus function as master
+  mb.begin(&S, DE_RE); //Assing Software serial port to Modbus instance for MAX485 chip having DI,DE,RE,RO Pin at TTL side
+  delay(2000);
+  Serial.print("MODBUSRTU_TIMEOUT = ");Serial.println(MODBUSRTU_TIMEOUT);
+ 
+};
+
+
+void loop() {
+  
+   if (!mb.slave()) {
+    int start_register=400;
+    mb.readHreg(cihaz_id,start_register, Mread0,my_array_size, cbWrite);
+    while (mb.slave()) {
+           mb.task();
+           delay(100);
+        };
+    Serial.println("------------------------------------------------");
+    Serial.println(error_code);
+    
+     delay(100);
+       if (error_code==0){
+    Serial.print("cihaz bulundu. BaudRate= ");Serial.print(myBaud[tarama_no]);
+    Serial.print(" ,party = ");Serial.print(myPar[0]);
+    Serial.print(" ,cihaz id = ");Serial.println(cihaz_id);
+    String mytext1=String(data_to_string("0-26",Mread0,my_array_size));
+    
+    }
+   else{
+    cihaz_id++;
+    if(cihaz_id==10){
+      cihaz_id=0;
+      tarama_no++;
+      
+      S.begin(myBaud[tarama_no],myPar[0]);
+      }
+    Serial.print("cihaz_id ");Serial.print(cihaz_id);
+    Serial.print(" myBaud ");Serial.print(myBaud[tarama_no]);
+    Serial.print(" par ");Serial.println(myPar[0]);
+    }
+  };
+  
+  delay(100);
+  yield();
+  
+
+}
